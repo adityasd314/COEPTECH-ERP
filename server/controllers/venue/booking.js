12cab24cc6ce98758cc828e2bb46a72b3262fe55
TanmayBansode <tanmaysbansode@gmail.com>
2024-04-10 20:54:27 +0530

    setup for venue backend

---
+++ server/controllers/venue/booking.js
@@ -0,0 +1,32 @@
+const grantBooking = () => {
+  return 0;
+};
+
+const revokeBooking = () => {
+  return 0;
+};
+
+const getAllBookings = () => {
+  return 0;
+};
+
+const makeBooking = () => {
+  return 0;
+};
+
+const cancelBooking = () => {
+  return 0;
+};
+
+const getMyBookings = () => {
+  return 0;
+};
+
+module.exports = {
+  grantBooking,
+  revokeBooking,
+  getAllBookings,
+  makeBooking,
+  cancelBooking,
+  getMyBookings,
+};
--- server/index.js
+++ server/index.js
@@ -9,10 +9,12 @@ app.use(helmet());
 app.use(express.json());
 
 const userRouter = require("./routes/user");
+const venueRouter = require("./routes/venue");
 
 // connectDB();
 
 app.use("/user", userRouter);
+app.use("/venue", venueRouter);
 
 app.listen(5000, () => {
     console.log("http://localhost:5000/");
