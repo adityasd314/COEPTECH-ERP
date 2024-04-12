cf2d8b8f5b76a84f6f814450f56204601376ff6c
Payal <payalss2004@gmail.com>
2024-04-12 16:44:12 +0530

    document-reports

--- server/controllers/venue/report.js
+++ server/controllers/venue/report.js
@@ -1,6 +1,28 @@
-const generateReport = () => {
-    return 0
+const { eq } = require('drizzle-orm');
+const DrizzleClient = require("../../lib/drizzle-client");
+const { reports } = require("../../db/schema");
+
+const storeReport = async(req, res) => {
+    try{
+        const {url, id} = req.body;
+        if(!url){
+            res.status(400).json({ error: 'All fields must be filled' });
+        }
+        const currentDate = new Date();
+        data = {
+            reportName: 'Permission Letter',
+            reportDate: currentDate,
+            reportType: 'letter',
+            generatedBy: id,
+            cloudinaryLink: url,
+        }
+        const report = await DrizzleClient.insert(reports).values(data).returning({
+            reportUrl : reports.cloudinaryLink
+        })
+        res.status(200).json({report})
+    }catch(error){
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 }
 
-
-module.exports = { generateReport };
+module.exports = { storeReport };
