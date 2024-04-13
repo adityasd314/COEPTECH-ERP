40f9be0e75881c7e99a2b43c6cd6d9a92cb321ac
TanmayBansode <tanmaysbansode@gmail.com>
2024-04-13 12:23:07 +0530

    query password login lms

--- lms-server/controllers/user/user.js
+++ lms-server/controllers/user/user.js
@@ -1,18 +1,60 @@
 const User = require("../../model/UserSchema");
 const jwt = require("jsonwebtoken");
+const { promisify } = require("util");
+const bcrypt = require("bcrypt");
+
+const hashPassword = async (password) => {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+};
+
+const compareHash = async (password, hash) => {
+  return await bcrypt.compare(password, hash);
+};
 
 const createToken = (_id) => {
   return jwt.sign({ _id }, "secret", { expiresIn: "3d" });
 };
 
-// login a user
-const loginUser = async (req, res) => {
-  const { email, password } = req.body;
+const loginWithToken = async (req, res) => {
+  const { token } = req.body;
 
   try {
+    const decodedToken = jwt.verify(token, "secret");
+
+    const userEmail = decodedToken.user.email;
+
+    const user = await User.findOne({ email: userEmail });
+  
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({ user });
+  } catch (err) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+};
+
+
+const loginUser = async (req, res) => {
+  
+  const { email, password } = req.body;
+
+  console.log(">> Email & Password", email, password);
+
+  if (!email || !password) {
+    res.status(400).json({ error: 'All fields must be filled' });
+    return;
+  }
+
+  
+  try {
+    
     const user = await User.login(email, password);
 
-    // create a token
+    console.log(">>", user);
+    
     const token = createToken(user._id);
 
     res.status(200).json({ ...user._doc, token });
@@ -59,4 +101,4 @@ const signupUser = async (req, res) => {
   }
 };
 
-module.exports = { loginUser };
+module.exports = { loginUser, loginWithToken };
